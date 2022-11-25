0d1112203b3e52bd06c5ff3dfcc301f3c7835863
torbik <liker1011@gmail.com>
2022-11-25 18:26:27 +0700

    Deploy to test in playground

--- src/instigators-nft.ts
+++ src/instigators-nft.ts
@@ -1,3 +1,4 @@
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   ApprovalForAll as ApprovalForAllEvent,
   DefaultRoyalty as DefaultRoyaltyEvent,
@@ -12,11 +13,12 @@ import {
   TokensMintedWithSignature as TokensMintedWithSignatureEvent,
   TransferBatch as TransferBatchEvent,
   TransferSingle as TransferSingleEvent,
-  URI as URIEvent
-} from "../generated/InstigatorsNFT/InstigatorsNFT"
+  URI as URIEvent,
+} from "../generated/InstigatorsNFT/InstigatorsNFT";
 import {
   ApprovalForAll,
   DefaultRoyalty,
+  AddressMinted,
   OwnerUpdated,
   PlatformFeeInfoUpdated,
   PrimarySaleRecipientUpdated,
@@ -28,50 +30,53 @@ import {
   TokensMintedWithSignature,
   TransferBatch,
   TransferSingle,
-  URI
-} from "../generated/schema"
+  URI,
+} from "../generated/schema";
+
+let ONE = BigInt.fromI32(1);
+let TWO = BigInt.fromI32(2);
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
   let entity = new ApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+  );
+  entity.account = event.params.account;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleDefaultRoyalty(event: DefaultRoyaltyEvent): void {
   let entity = new DefaultRoyalty(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.newRoyaltyRecipient = event.params.newRoyaltyRecipient
-  entity.newRoyaltyBps = event.params.newRoyaltyBps
+  );
+  entity.newRoyaltyRecipient = event.params.newRoyaltyRecipient;
+  entity.newRoyaltyBps = event.params.newRoyaltyBps;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnerUpdated(event: OwnerUpdatedEvent): void {
   let entity = new OwnerUpdated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.prevOwner = event.params.prevOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.prevOwner = event.params.prevOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handlePlatformFeeInfoUpdated(
@@ -79,15 +84,15 @@ export function handlePlatformFeeInfoUpdated(
 ): void {
   let entity = new PlatformFeeInfoUpdated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.platformFeeRecipient = event.params.platformFeeRecipient
-  entity.platformFeeBps = event.params.platformFeeBps
+  );
+  entity.platformFeeRecipient = event.params.platformFeeRecipient;
+  entity.platformFeeBps = event.params.platformFeeBps;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handlePrimarySaleRecipientUpdated(
@@ -95,90 +100,90 @@ export function handlePrimarySaleRecipientUpdated(
 ): void {
   let entity = new PrimarySaleRecipientUpdated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.recipient = event.params.recipient
+  );
+  entity.recipient = event.params.recipient;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
   let entity = new RoleAdminChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
+  );
+  entity.role = event.params.role;
+  entity.previousAdminRole = event.params.previousAdminRole;
+  entity.newAdminRole = event.params.newAdminRole;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleGranted(event: RoleGrantedEvent): void {
   let entity = new RoleGranted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.role = event.params.role;
+  entity.account = event.params.account;
+  entity.sender = event.params.sender;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleRevoked(event: RoleRevokedEvent): void {
   let entity = new RoleRevoked(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.role = event.params.role;
+  entity.account = event.params.account;
+  entity.sender = event.params.sender;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoyaltyForToken(event: RoyaltyForTokenEvent): void {
   let entity = new RoyaltyForToken(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.royaltyRecipient = event.params.royaltyRecipient
-  entity.royaltyBps = event.params.royaltyBps
+  );
+  entity.tokenId = event.params.tokenId;
+  entity.royaltyRecipient = event.params.royaltyRecipient;
+  entity.royaltyBps = event.params.royaltyBps;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTokensMinted(event: TokensMintedEvent): void {
   let entity = new TokensMinted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.mintedTo = event.params.mintedTo
-  entity.tokenIdMinted = event.params.tokenIdMinted
-  entity.uri = event.params.uri
-  entity.quantityMinted = event.params.quantityMinted
+  );
+  entity.mintedTo = event.params.mintedTo;
+  entity.tokenIdMinted = event.params.tokenIdMinted;
+  entity.uri = event.params.uri;
+  entity.quantityMinted = event.params.quantityMinted;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTokensMintedWithSignature(
@@ -186,76 +191,113 @@ export function handleTokensMintedWithSignature(
 ): void {
   let entity = new TokensMintedWithSignature(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.signer = event.params.signer
-  entity.mintedTo = event.params.mintedTo
-  entity.tokenIdMinted = event.params.tokenIdMinted
-  entity.mintRequest_to = event.params.mintRequest.to
+  );
+  entity.signer = event.params.signer;
+  entity.mintedTo = event.params.mintedTo;
+  entity.tokenIdMinted = event.params.tokenIdMinted;
+  entity.mintRequest_to = event.params.mintRequest.to;
   entity.mintRequest_royaltyRecipient =
-    event.params.mintRequest.royaltyRecipient
-  entity.mintRequest_royaltyBps = event.params.mintRequest.royaltyBps
+    event.params.mintRequest.royaltyRecipient;
+  entity.mintRequest_royaltyBps = event.params.mintRequest.royaltyBps;
   entity.mintRequest_primarySaleRecipient =
-    event.params.mintRequest.primarySaleRecipient
-  entity.mintRequest_tokenId = event.params.mintRequest.tokenId
-  entity.mintRequest_uri = event.params.mintRequest.uri
-  entity.mintRequest_quantity = event.params.mintRequest.quantity
-  entity.mintRequest_pricePerToken = event.params.mintRequest.pricePerToken
-  entity.mintRequest_currency = event.params.mintRequest.currency
+    event.params.mintRequest.primarySaleRecipient;
+  entity.mintRequest_tokenId = event.params.mintRequest.tokenId;
+  entity.mintRequest_uri = event.params.mintRequest.uri;
+  entity.mintRequest_quantity = event.params.mintRequest.quantity;
+  entity.mintRequest_pricePerToken = event.params.mintRequest.pricePerToken;
+  entity.mintRequest_currency = event.params.mintRequest.currency;
   entity.mintRequest_validityStartTimestamp =
-    event.params.mintRequest.validityStartTimestamp
+    event.params.mintRequest.validityStartTimestamp;
   entity.mintRequest_validityEndTimestamp =
-    event.params.mintRequest.validityEndTimestamp
-  entity.mintRequest_uid = event.params.mintRequest.uid
+    event.params.mintRequest.validityEndTimestamp;
+  entity.mintRequest_uid = event.params.mintRequest.uid;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  let existingAddressMintedRecord = checkAddressMinted(event);
+
+  if (!!existingAddressMintedRecord) {
+    updateAddressMinted(existingAddressMintedRecord);
+  } else {
+    createAddressMinted(event);
+  }
 }
 
 export function handleTransferBatch(event: TransferBatchEvent): void {
   let entity = new TransferBatch(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.ids = event.params.ids
-  entity.values = event.params.values
+  );
+  entity.operator = event.params.operator;
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.ids = event.params.ids;
+  entity.values = event.params.values;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
   let entity = new TransferSingle(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.id = event.params.id
-  entity.value = event.params.value
+  );
+  entity.operator = event.params.operator;
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.trId = event.params.id;
+  entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleURI(event: URIEvent): void {
-  let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
-  entity.value = event.params.value
-  entity.id = event.params.id
+  let entity = new URI(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  );
+  entity.value = event.params.value;
+  entity.uriId = event.params.id;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+}
+
+function createAddressMinted(event: TokensMintedWithSignatureEvent): void {
+  let entity = new AddressMinted(
+    event.params.mintedTo.concatI32(event.logIndex.toI32())
+  );
+
+  entity.entries;
+  entity.address = event.params.mintedTo;
+  entity.numberMinted = ONE;
+
+  entity.save();
+}
+
+function updateAddressMinted(entity: AddressMinted): void {
+  entity.numberMinted = TWO;
+  entity.save();
+}
+
+function checkAddressMinted(
+  event: TokensMintedWithSignatureEvent
+): AddressMinted | null {
+  let entity = AddressMinted.load(
+    event.params.mintedTo.concatI32(event.logIndex.toI32())
+  );
+
+  return !!entity ? entity : null;
 }
